26a5e4159bf58cb83bebc4d506b48018610bb132
abdou <002.abdou@gmail.com>
2022-10-12 12:40:35 +0100

    Auto-generated commit [ci skip]

---
+++ assets/js/observers.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function () {
+  console.log("ready!");
+  const header = document.querySelector("header");
+const sectionOne = document.querySelector(".home-intro");
+
+const sectionOneOptions = {
+  rootMargin: "-200px 0px 0px 0px"
+};
+  const options = {};
+
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  }, options);
+},
+sectionOneOptions);
+
+  sectionOneObserver.observe(sectionOne);
+});
+
+
